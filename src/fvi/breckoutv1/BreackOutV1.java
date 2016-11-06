b5500903f415f39724dcb2f23e8efea508674829
Victoria Frantsuh <eboko1@gmail.com>
2016-11-06 16:04:35 +0200

    Initial commit

---
+++ src/fvi/breckoutv1/BreackOutV1.java
@@ -0,0 +1,27 @@
+package fvi.breckoutv1;
+
+/**
+ * Created by Vika on 06.11.2016.
+ */
+public class BreackOutV1 {
+    //application window in pixel
+    public  static  final int APPLICATION_WIDTH=400;
+    public  static  final int APPLICATION_HEIGHT=600;
+
+    // dimensions of game board
+    public  static  final int WIDTH=APPLICATION_WIDTH;
+    public  static  final int HEIGHT=APPLICATION_HEIGHT;
+
+    //Dimensions of paddle up from the bottom
+    public  static  final int PADDLE_WIDTH=60;
+    public  static  final int PADDLE_HEIGTH=10;
+
+    //offset of the paddle up from the bottom
+    public  static  final int PADDLE_Y_OFFSET=30;
+
+    //number of bricks per row
+    public  static  final int NBRICKS_PER_ROW=10;
+
+    // number of rows of bricks
+    public  static  final int NBRICK_ROWS=10;
+}
